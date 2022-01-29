0688a4bdc587d0647350b0ab936c61eb72c0ddaa
Patrick Roos <pa.roos@icloud.com>
2022-01-29 11:30:16 +0100

    technolgoy-radar-web-components exercise added

---
+++ exercises/technology-radar-web-components/components/techradar-add-technology.js
@@ -0,0 +1,18 @@
+class TechradarAddTechnology extends HTMLElement {
+    constructor() {
+      super();
+      this.shadowDom = this.attachShadow({mode: 'open'});
+      this.shadowDom.innerHTML = `
+
+      `;
+    }
+
+    // Web Component connected
+    connectedCallback() {
+    
+    }
+
+  }
+  window.customElements.define('techradar-add-technology', TechradarAddTechnology);
+  
+
---
+++ exercises/technology-radar-web-components/components/techradar-list-technologies.js
@@ -0,0 +1,18 @@
+class TechradarListTechnologies extends HTMLElement {
+    constructor() {
+        super();
+
+        this.shadowDom = this.attachShadow({ mode: 'open' });
+        this.shadowDom.innerHTML = `
+
+      `;
+
+      this.renderTechnologies();
+    }
+
+    // Web Component connected
+    connectedCallback() { 
+
+    }
+}
+window.customElements.define('techradar-list-technologies', TechradarListTechnologies);
