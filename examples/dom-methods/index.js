84285ffd6ada9753cce4822b301468c53293efb8
Patrick Roos <pa.roos@icloud.com>
2023-01-30 21:19:18 +0100

    Add comments to dom-methods examples

--- examples/dom-methods/index.js
+++ examples/dom-methods/index.js
@@ -1,19 +1,27 @@
 /* eslint-disable quotes */
 console.log(
   "querySelector('ul > li:last-child')",
-  document.querySelector('ul > li:last-child')
+  document.querySelector('ul > li:last-child') // HTMLLIElement
 );
 console.log(
   "querySelectorAll('ul > li')",
-  document.querySelectorAll('ul > li')
+  document.querySelectorAll('ul > li') // NodeList
 );
-console.log("getElementById('myList')", document.getElementById('myList'));
+console.log(
+  "getElementById('myList')",
+  document.getElementById('myList') // HTMLUListElement
+);
 console.log(
   "getElementsByName('myListName')",
-  document.getElementsByName('myListName')
+  document.getElementsByName('myListName') // NodeList
 );
-console.log("getElementsByTagName('li')", document.getElementsByTagName('li')); // Returns a HTMLCollection (because it contains only element nodes (tags))
+console.log("getElementsByTagName('li')", document.getElementsByTagName('li')); // Returns a HTMLCollection (because it can contain only element nodes (tags))
 console.log(
   "getElementsByClass('myClass')",
   document.getElementsByClassName('myClass')
-); // Returns a HTMLCollection (because it contains only element nodes (tags))
+); // Returns a HTMLCollection (because it can contain only element nodes (tags))
+
+/**
+ * An HTMLCollection is always a live collection. Example: If you add a <li> element to a list in the DOM, the list in the HTMLCollection will also change.
+ * A NodeList is most often a static collection. Example: If you add a <li> element to a list in the DOM, the list in NodeList will not change.
+ */
