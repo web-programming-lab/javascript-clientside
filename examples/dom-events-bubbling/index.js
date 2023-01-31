e714de35f23412ea6308240c6e8dee09c172e379
Patrick Roos <pa.roos@icloud.com>
2023-01-31 21:08:33 +0100

    Make Capturing explicit via attribute

--- examples/dom-events-bubbling/index.js
+++ examples/dom-events-bubbling/index.js
@@ -1,7 +1,7 @@
 document.getElementById('child').addEventListener('click', clickHandler);
 document
   .getElementById('parent')
-  .addEventListener('click', clickHandler, false);
+  .addEventListener('click', clickHandler, { capture: false });
 
 function clickHandler(event) {
   console.log(event.type, event);
