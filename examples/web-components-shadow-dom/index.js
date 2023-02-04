b668d7665a0c0e50525f739d80d0bce1d983d8f4
Patrick Roos <pa.roos@icloud.com>
2023-02-04 16:37:03 +0100

    Lifecycle callbackes added

--- examples/web-components-shadow-dom/index.js
+++ examples/web-components-shadow-dom/index.js
@@ -5,6 +5,22 @@ class MyCustomElement extends HTMLElement {
     shadowDom.innerHTML =
       '<style>p {color: blue}</style><p>this is my custom element</p>';
   }
+
+  // Lifecycle Callbacks
+  // Wird aufgerufen wenn das Custom Element dem DOM hinzugefügt wird
+  connectedCallback() {
+    console.log('custom element added to page');
+  }
+
+  // Wird aufgerufen wenn das Custom Element dem DOM entfernt wird
+  disconnectedCallback() {
+    console.log('custom element removed from page');
+  }
+
+  // Wird aufgerufen wenn ein Attribut des Custom Elements geändert wird
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('custom element attributes changed', name, oldValue, newValue);
+  }
 }
 window.customElements.define('my-custom-element', MyCustomElement);
 
