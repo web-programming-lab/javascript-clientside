e7e5d48effc6739aec9dcee969c883407dc1e089
Patrick Roos <pa.roos@icloud.com>
2023-02-07 08:00:06 +0100

    Web Components Event Example added

---
+++ examples/web-components-events/index.js
@@ -0,0 +1,37 @@
+class MyCustomElementOne extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDom = this.attachShadow({ mode: 'open' });
+    this.shadowDom.innerHTML = `
+      <p>this is my custom element 1</p>
+      <button id="exampleButton">Button</button>
+      `;
+  }
+
+  connectedCallback() {
+    const buttonEl = this.shadowDom.querySelector('#exampleButton');
+    buttonEl.addEventListener('click', () => {
+      this.dispatchEvent(new Event('customelement-one-click'));
+    });
+  }
+}
+
+class MyCustomElementTwo extends HTMLElement {
+  constructor() {
+    super();
+    this.shadowDom = this.attachShadow({ mode: 'open' });
+    this.shadowDom.innerHTML = '<p>this is my custom element 2</p>';
+  }
+
+  connectedCallback() {
+    const customElementOneEl = document.querySelector('my-custom-element-one');
+    customElementOneEl.addEventListener('customelement-one-click', () => {
+      this.shadowDom.innerHTML = `${
+        this.shadowDom.innerHTML
+      } <p>${new Date()}</p>`;
+    });
+  }
+}
+
+window.customElements.define('my-custom-element-one', MyCustomElementOne);
+window.customElements.define('my-custom-element-two', MyCustomElementTwo);
